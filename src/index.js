85bb49d3beefe5d93c158acc73025510d5f510bc
Anastasiya <nastyapopovaa1@gmail.com>
2022-06-24 23:25:46 +0300

    feat: add solution of the task

--- src/index.js
+++ src/index.js
@@ -10,26 +10,28 @@ module.exports = function toReadable (number) {
     res = (res + '').split('.');
     res = Number(res[1]);
 
-    let numToNineteen = ['', 'one' , 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourten', 'fifteen', 'sixteen', 'seventen', 'eighteen', 'nineteen'];
-    let numTens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'sevety', 'eighty', 'ninety'];
+    let numToNineteen = ['', 'one' , 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    let numTens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
     let numScales = ['', '', '', 'hundred', 'thousand'];
 
     // Проверяем числа от 1 до 19
     if(str.length <= 2 && res <= 19){
-        return res = numToNineteen[number];
+        res = numToNineteen[number];
     };
 
     // Проверяем числа до 99
     if(str.length == 2 && res >= 19){
-        return res = `${numTens[str[0]]} ${numToNineteen[str[1]]}`;
+        res = `${numTens[str[0]]} ${numToNineteen[str[1]]}`;
     };
 
     // Проверяем числа до 999
     if(str.length > 2 && res <= 19){
-        return res = `${numToNineteen[str[0]]} ${numScales[str.length]} ${numToNineteen[res]}`;
+        res = `${numToNineteen[str[0]]} ${numScales[str.length]} ${numToNineteen[res]}`;
     };
 
     if(str.length > 2 && res > 19){
-        return res = `${numToNineteen[str[0]]} ${numScales[str.length]} ${numTens[str[str.length - 2]]} ${numToNineteen[str[str.length - 1]]}`;
+        res = `${numToNineteen[str[0]]} ${numScales[str.length]} ${numTens[str[str.length - 2]]} ${numToNineteen[str[str.length - 1]]}`;
     }
+
+    return res.trimEnd();
 }
