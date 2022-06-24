fff0bae58853df322542c83a09b87097f3e19e36
Anastasiya <nastyapopovaa1@gmail.com>
2022-06-24 23:14:47 +0300

    feat: add the solution of the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function toReadable (number) {
-    
+    let str = number.toString();
+
+    // Проверяем число 0
+    if(str === '0') {
+        return 'zero';
+    };
+
+    let res = (number / 100).toFixed(2);
+    res = (res + '').split('.');
+    res = Number(res[1]);
+
+    let numToNineteen = ['', 'one' , 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourten', 'fifteen', 'sixteen', 'seventen', 'eighteen', 'nineteen'];
+    let numTens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'sevety', 'eighty', 'ninety'];
+    let numScales = ['', '', '', 'hundred', 'thousand'];
+
+    // Проверяем числа от 1 до 19
+    if(str.length <= 2 && res <= 19){
+        return res = numToNineteen[number];
+    };
+
+    // Проверяем числа до 99
+    if(str.length == 2 && res >= 19){
+        return res = `${numTens[str[0]]} ${numToNineteen[str[1]]}`;
+    };
+
+    // Проверяем числа до 999
+    if(str.length > 2 && res <= 19){
+        return res = `${numToNineteen[str[0]]} ${numScales[str.length]} ${numToNineteen[res]}`;
+    };
+
+    if(str.length > 2 && res > 19){
+        return res = `${numToNineteen[str[0]]} ${numScales[str.length]} ${numTens[str[str.length - 2]]} ${numToNineteen[str[str.length - 1]]}`;
+    }
 }
